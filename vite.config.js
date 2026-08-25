d359d12782283fe943869a2eaacf8e8cf3cb6ffd
Leonardo Arruda <leonardoaugusto.info@gmail.com>
2026-08-25 14:51:44 -0300

    Change base path for deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/leomagro-site/',
+  base: '/leomagro.github.io/',
 })
